dc2b21d4e76fd2c241ff5f1077f04c0ebc3c3598
Naaz Manhas <79596294+pumpumx@users.noreply.github.com>
2024-10-17 16:49:12 +0530

    Update 1.cpp

--- CipherSchools/1.cpp
+++ CipherSchools/1.cpp
@@ -17,9 +17,33 @@ int factorial(int n){
    return badi;
 
 }
+
+
+
 int main(){
     int n;
     cin>>n;
     int temp = n;
     cout<<factorial(n);
 }
+
+/* Sum using recursion
+
+int sum1(int *arr,int n,int sum){
+    sum+=arr[0];
+    if(n==0){
+        return sum;
+    }
+    sum1(arr+1,n-1,sum);
+
+}
+int main(){
+    int n,sum=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<sum1(arr,n,sum);
+}
+*/
