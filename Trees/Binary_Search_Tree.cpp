5a0b1241920a246c89557391e6b457db083caf44
pumpumx <trishuchibhot14@gmail.com>
2024-11-21 21:52:41 +0530

    BST

---
+++ Trees/Binary_Search_Tree.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+                        //For every node , the data on left will be smaller and the data on right will be bigger than the root node.
+class Tree{
+    public:
+        int val;
+        Tree *left , *right;
+        left
+}
