cff94e0133e768556c7265f63a5d90f71fd1d2ac
pumpumx <trishuchibhot14@gmail.com>
2024-12-05 02:02:18 +0530

    Permutation_in_string

---
+++ Recursion/Permutations_in_a_string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+
+void permutation(string str , vector<string> &ans , int index){
+    if(index>=str.size()){
+        ans.push_back(str);
+        return;
+    }
+    for(int j=index;j<str.size();j++){
+        swap(str[index],str[j]);
+        permutation(str,ans,index+1);
+        swap(str[index],str[j]); //Backtrack
+    }
+}
+int main(){
+    string str;
+    getline(cin,str);
+    vector<string> ans;
+    int index =0;
+    permutation(str,ans,index);
+    for(auto num:ans){
+        cout<<num<<" ";
+    }
+
+}
