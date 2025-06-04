ac471732d6a5043273991ff2713a294075dee98d
pumpumx <trishuchibhot14@gmail.com>
2025-06-04 08:02:57 +0530

    lets go brrrr

--- StacksAndQueues/1.cpp
+++ StacksAndQueues/1.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main(){
 
-    
+class Stack{
+    private:
+        int arr[100];
+        int currSize = 0;
+        int top = -1;
+    public: 
+        void push(int x){
+            arr[currSize] = x;
+            top = currSize;
+            currSize++;
+        }
+        void pop(){
+            int p * = arr[currSize];
+            arr[currSize]--;
+            top = currSize;
+            delete p;
+        }
+        int size(){
+            return currSize; 
+        }
+        int top(){
+            return top; 
+        }
+        
+
+}
+int main(){ //Implementing stack using array
+
+    int stack = new Stack()
+
+
 }
